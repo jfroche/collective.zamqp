758962c13c43231341798e21c012f3340f5728cb
Jean-François Roche <jfroche@affinitic.be>
2011-06-14 16:43:54 +0200

    remove logging definition from code, should be in config file

--- src/affinitic/zamqp/__init__.py
+++ src/affinitic/zamqp/__init__.py
@@ -1,11 +1,3 @@
 import logging
-from logging.handlers import TimedRotatingFileHandler
 LOGGER = 'affinitic.zamqp'
 logger = logging.getLogger(LOGGER)
-logger.setLevel(logging.DEBUG)
-fh = TimedRotatingFileHandler("amqp.log", 'midnight', 1)
-fh.suffix = "%Y-%m-%d-%H-%M"
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-fh.setFormatter(formatter)
-fh.setLevel(logging.DEBUG)
-logger.addHandler(fh)
